f3a7f35f8c940f3e2aff78c44aa4639750183db4
grenewode <grenewode@pm.me>
2023-09-03 01:23:34 -0400

    add deploy-rs to environment

--- configuration.nix
+++ configuration.nix
@@ -1,10 +1,38 @@
-{ self, pkgs, config, lib, modulesPath, ... }: {
+{ self, pkgs, config, lib, modulesPath, ... }:
+let
+  inherit (lib) mkIf;
+  inherit (config.services) jupyterhub nginx;
+  inherit (config) networking;
+  inherit (pkgs) openssl python312 nodejs symlinkJoin;
+
+  python3 = python312;
+
+  pythonEnv = python3.withPackages ({ pipx, pip, ... }: [ pipx pip ]);
+in
+{
 
   imports = [ (modulesPath + "/virtualisation/proxmox-lxc.nix") ];
 
+  nix = {
+    nixPath = [
+      "nixpkgs=${self.inputs.nixpkgs}"
+      "nixos-config=${./configuration.nix}"
+    ];
+  };
+
+  proxmoxLXC = {
+    privileged = false;
+    manageNetwork = false;
+    manageHostName = true;
+  };
+
   nixpkgs.hostPlatform = "x86_64-linux";
 
-  networking = { domain = "intra.lair.onl"; };
+  networking = {
+    hostName = "jupyter";
+
+    domain = "intra.lair.onl";
+  };
 
   system.autoUpgrade = {
     enable = true;
@@ -16,21 +44,83 @@
     };
   };
 
-  proxmoxLXC = {
-    privileged = false;
-    manageNetwork = false;
-    manageHostName = false;
-  };
-
-  services.cloud-init.network.enable = true;
-
   services.jupyterhub = {
     enable = true;
 
-    jupyterlabEnv = pkgs.python3.withPackages
-      (p: with p; [ jupyterhub jupyterlab jupyterlab-lsp python-lsp-server ]);
+    host = "127.0.0.1";
 
+    jupyterlabEnv = python3.withPackages (p: with p; [ jupyterhub jupyterlab ]);
+    jupyterhubEnv = python3.withPackages
+      (p: with p; [ jupyterhub jupyterhub-systemdspawner ]);
+
+    extraConfig = ''
+      c.SystemdSpawner.isolate_tmp = True
+      c.SystemdSpawner.isolate_devices = True
+      c.SystemdSpawner.readonly_paths = ['/']
+      c.SystemdSpawner.readwrite_paths = [ '/home/{USERNAME}', '/nix/store', '/nix/var/nix/profiles/per-user/{USERNAME}' ]
+      c.SystemdSpawner.disable_user_sudo = True
+      c.SystemdSpawner.unit_extra_properties = { 'PrivateMounts': True }
+      c.SystemdSpawner.extra_paths = [ "${pythonEnv}/bin" ]
+    '';
   };
+
+  services.nginx = mkIf jupyterhub.enable ({
+    enable = true;
+    recommendedOptimisation = true;
+    recommendedGzipSettings = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+
+    preStart = ''
+      KEY_FILE="$STATE_DIRECTORY/certs/${networking.fqdn}.key"
+      CERT_FILE="$STATE_DIRECTORY/certs/${networking.fqdn}.cert"
+
+      if [[ ! -f "$CERT_FILE" ]];
+      then
+          mkdir -p "$STATE_DIRECTORY/certs"
+          ${openssl}/bin/openssl req \
+                                 -newkey rsa:2048 -nodes -keyout "$KEY_FILE" \
+                                 -x509 -out "$CERT_FILE" \
+                                 -subj "/CN=${networking.fqdn}"
+      fi
+    '';
+
+    virtualHosts."${networking.fqdn}" = {
+      forceSSL = true;
+
+      sslCertificateKey = "/var/lib/nginx/certs/${networking.fqdn}.key";
+      sslCertificate = "/var/lib/nginx/certs/${networking.fqdn}.cert";
+
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:${toString jupyterhub.port}";
+        proxyWebsockets = true;
+
+        extraConfig = ''
+          proxy_set_header X-Scheme $scheme;
+          proxy_buffering off;
+        '';
+      };
+
+      locations."~ /.well-known" = {
+        extraConfig = ''
+          allow all;
+        '';
+      };
+    };
+  });
+
+  networking.firewall = mkIf nginx.enable { allowedTCPPorts = [ 80 443 ]; };
+
+  systemd.services.nginx = mkIf nginx.enable {
+    serviceConfig.StateDirectory = "nginx";
+    serviceConfig.StateDirectoryMode = "0750";
+  };
+
+  systemd.suppressedSystemUnits = [
+    "dev-mqueue.mount"
+    "sys-kernel-debug.mount"
+    "sys-fs-fuse-connections.mount"
+  ];
 
   users.mutableUsers = false;
   users.users.root = {
@@ -40,5 +130,11 @@
     ];
   };
 
-  system.stateVersion = "22.11";
+  users.users.grenewode = {
+    isNormalUser = true;
+    hashedPassword =
+      "$y$j9T$JvhWo6iJguPWNuFcjCv300$pIEkwdqQ82A3N54k4NWl5wNcktIo5VaQd7bfwr2G.e0";
+  };
+
+  system.stateVersion = "23.05";
 }
--- flake.nix
+++ flake.nix
@@ -1,28 +1,58 @@
 {
   description = "A very basic flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
   inputs.nixos-generators = {
     url = "github:nix-community/nixos-generators";
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixos-generators }: rec {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix ];
+  inputs.deploy-rs.url = "github:serokell/deploy-rs";
 
-      specialArgs = { inherit self; };
-    };
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-    packages.x86_64-linux = {
-      default = nixos-generators.nixosGenerate {
-        system = "x86_64-linux";
-
+  outputs = { self, nixpkgs, nixos-generators, deploy-rs, flake-utils }:
+    {
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
         modules = [ ./configuration.nix ];
 
-        format = "proxmox-lxc";
+        specialArgs = { inherit self; };
       };
-    };
-  };
+
+      deploy.nodes."jupyter.intra.lair.onl" = {
+
+        hostname = "jupyter.intra.lair.onl";
+        profiles.system = {
+          user = "root";
+          sshUser = "root";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.default;
+        };
+      };
+
+      checks =
+        builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy)
+          deploy-rs.lib;
+    } // (flake-utils.lib.eachSystem [ self.nixosConfigurations.default.config.nixpkgs.hostPlatform.system] (localSystem:
+      let
+        pkgs = import nixpkgs { inherit localSystem; };
+        inherit (pkgs) mkShell deploy-rs;
+      in
+      {
+
+        devShells.default = mkShell { packages = [ deploy-rs ]; };
+
+        packages.default =
+          (nixos-generators.nixosGenerate {
+            system = localSystem;
+
+            modules = [ ./configuration.nix ];
+
+            specialArgs = { inherit self; };
+
+            format = "proxmox-lxc";
+          }).override {
+            fileName = "nixos-23.05-jupyterhub_${self.lastModifiedDate}_amd64";
+          };
+      }));
 }
